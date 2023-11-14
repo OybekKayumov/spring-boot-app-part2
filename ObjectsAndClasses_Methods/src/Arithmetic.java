9ff3de9ee00a3ba1bf6cda29c00b9c377fff0021
OybekKayumov <oybek.kayumov@gmail.com>
2023-11-14 08:42:16 +0500

    Add task 1

--- ObjectsAndClasses_Methods/src/Arithmetic.java
+++ ObjectsAndClasses_Methods/src/Arithmetic.java
@@ -1,3 +1,26 @@
 public class Arithmetic {
 
+    private int number1 = 4;
+    private int number2 = 5;
+
+    public void Arithmetic(int number1, int number2) {
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    public int sum(int a, int b){
+        return a + b;
+    }
+
+    public int multiple(int a, int b){
+        return a * b;
+    }
+
+    public int maxOfTwo(int a, int b){
+        return Math.max(a, b);
+    }
+
+    public int minOfTwo(int a, int b){
+        return Math.min(a, b);
+    }
 }
