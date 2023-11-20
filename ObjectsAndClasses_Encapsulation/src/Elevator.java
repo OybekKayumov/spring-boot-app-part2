3796c5f29b790e286d8a89bb3c66e90c4312ca20
OybekKayumov <oybek.kayumov@gmail.com>
2023-11-20 09:27:33 +0500

    Add Elevator and Main classes

--- ObjectsAndClasses_Encapsulation/src/Elevator.java
+++ ObjectsAndClasses_Encapsulation/src/Elevator.java
@@ -2,8 +2,41 @@
 
 public class Elevator {
 
-    public static void main(String[] args) {
+   private int currentFloor = 1;
+   private int minFloor;
+   private int maxFloor;
 
-        System.out.println("Hi");
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void moveDown() {
+        currentFloor = currentFloor > minFloor ? currentFloor - 1 : currentFloor;
+    }
+
+    public void moveUp() {
+        currentFloor = currentFloor < maxFloor? currentFloor + 1 : currentFloor;
+    }
+
+    public void move(int floor) {
+        if (floor < minFloor || floor > maxFloor) {
+            System.out.println("Извините, нет такого этажа");
+        } else if (floor == currentFloor) {
+            System.out.println("Текущий этаж: " + floor);
+        } else {
+            while (floor > currentFloor) {
+                moveUp();
+                System.out.println("Текущий этаж: " + currentFloor);
+            }
+            while (floor < currentFloor) {
+                moveDown();
+                System.out.println("Текущий этаж: " + currentFloor);
+            }
+        }
     }
 }
